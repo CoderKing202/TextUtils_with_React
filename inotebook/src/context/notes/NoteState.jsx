e16f05ce4674ffc6ec1512b9bcd486ccd4057da3
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2025-12-08 16:11:32 +0530

    Lesson 60 iNotebook: Fetching Notes from Context

--- inotebook/src/context/notes/NoteState.jsx
+++ inotebook/src/context/notes/NoteState.jsx
@@ -2,13 +2,38 @@ import NoteContext from "./noteContext";
 import { useContext, useState } from "react";
 
 const NoteState = (props)=>{
-    
-    const [state,setState] = useState()
+    const notesInitial = [
+  {
+    "_id": "691d8750a94588d097700dc0",
+    "user": "6915b04826a5c49f0d7879d2",
+    "title": "New Note",
+    "description": "Please access the playlist",
+    "tag": "Youtube",
+    "__v": 0
+  },
+  {
+    "_id": "6921a6822f819714251f2e38",
+    "user": "6915b04826a5c49f0d7879d2",
+    "title": "New Note",
+    "description": "Please access the playlist",
+    "tag": "Youtube",
+    "__v": 0
+  },
+  {
+    "_id": "6921a6932f819714251f2e3c",
+    "user": "6915b04826a5c49f0d7879d2",
+    "title": "New Note 3",
+    "description": "Please access the playlist",
+    "tag": "Youtube",
+    "__v": 0
+  }
+]
+    const [notes,setNotes] = useState(notesInitial)
     const update = ()=>{
    
     }
    return(
-    <NoteContext.Provider value={{}}>
+    <NoteContext.Provider value={{notes,setNotes}}>
         {props.children}
     </NoteContext.Provider>
    )  
