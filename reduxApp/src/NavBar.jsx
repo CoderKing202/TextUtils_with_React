913cb79c70ca4f2e45b3ef0c7c9603bb790bd0e8
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2025-12-30 22:16:54 +0530

    Lesson 77 Accessing State in Redux

--- reduxApp/src/NavBar.jsx
+++ reduxApp/src/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function NavBar() {
+  // const amount = useSelector(state => state.amount)
   return (
     <div><nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
